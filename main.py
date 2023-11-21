83a569d871c93ab1576912e4508d0c4e92809fac
Tae Min Ha <taemin410@gmail.com>
2023-11-21 20:07:08 +0800

    remove version

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI() # This is what will be refrenced in config
 
 @app.get('/ping')
 async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+    return {'res': 'pong', "time": time()}
